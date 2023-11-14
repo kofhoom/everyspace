26ce01eae82fba1b25cf55638bbf6ce90f51e90d
Kim Jun Su <64389254+kofhoom@users.noreply.github.com>
2023-11-15 03:15:39 +0900

    빌드 경로문제 수정

--- client/pages/admin/login/index.tsx
+++ client/pages/admin/login/index.tsx
@@ -1,4 +1,4 @@
-import LoginListPage from "@/src/components/units/login/Login.index";
+import LoginListPage from "../../../src/components/units/login/Login.index";
 
 // 어드민 로그인 페이지
 export default function LoginPage() {
