b3637c3c081b7a2b1d2147ed9d2ac85d1bfebbf3
Kim Jun Su <64389254+kofhoom@users.noreply.github.com>
2023-11-17 15:01:38 +0900

    클라이언트 빌드13

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -4,13 +4,13 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "ec2-43-201-38-34.ap-northeast-2.compute.amazonaws.com",
+  host: "db",
   port: 5432,
   username: "postgres",
   password: "password",
   database: "postgres",
   synchronize: true,
-  logging: false,
+  logging: true,
   entities: ["src/entities/**/*.ts"],
   migrations: [],
   subscribers: [],
